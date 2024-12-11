6f6eedd94c17b1e67865c70206881aaaaa58054a
Issac-Ho <issac.ho@edu.sait.ca>
2024-12-10 23:49:16 -0700

    last commit

--- app/page.js
+++ app/page.js
@@ -6,10 +6,10 @@ export default function Home() {
   return (
     <div className="bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 min-h-screen">
       <main className="p-10 max-w-4xl mx-auto">
-        <h1 className="text-3xl font-bold mb-4">Hi, I'm Issac Ho</h1>
+        <h1 className="text-3xl font-bold mb-4">Hi, I am Issac Ho</h1>
         <h2 className="text-2xl font-semibold mb-4">Welcome to My Introduction Page</h2>
         <p className="leading-7">
-          Explore the links above to learn more about me, my skills, the projects I've worked on, and how to get in touch. 
+          Explore the links above to learn more about me, my skills, the projects I have worked on, and how to get in touch. 
         </p>
       </main>
       <Footer /> 
